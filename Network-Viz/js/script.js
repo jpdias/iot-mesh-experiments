cdc2bb6bd0beda373b998792081c97bf982d2d18
jpdias <jpdias@live.com.pt>
2017-07-28 18:03:29 +0100

    WIP on viz

--- Network-Viz/js/script.js
+++ Network-Viz/js/script.js
@@ -1,86 +1,115 @@
-var cy = cytoscape({
-    container: document.getElementById('cy'),
+const cy = cytoscape({
+  container: document.getElementById('cy'),
 
-    boxSelectionEnabled: false,
-    autounselectify: true,
+  boxSelectionEnabled: false,
+  autounselectify: true,
 
-    style: cytoscape.stylesheet()
-        .selector('node')
-        .css({
-            'content': 'data(id)'
-        })
-        .selector('edge')
-        .css({
-            'curve-style': 'bezier',
-            'target-arrow-shape': 'triangle',
-            'width': 4,
-            'line-color': '#ddd',
-            'target-arrow-color': '#ddd'
-        })
-        .selector('.highlighted')
-        .css({
-            'background-color': '#61bffc',
-            'line-color': '#61bffc',
-            'target-arrow-color': '#61bffc',
-            'transition-property': 'background-color, line-color, target-arrow-color',
-            'transition-duration': '0.5s'
-        }),
-
-    layout: {
-        name: 'breadthfirst',
-        directed: true,
-        roots: '#a',
-        padding: 10
-    }
+  style: cytoscape.stylesheet()
+    .selector('node')
+    .css({
+      content: 'data(id)',
+    })
+    .selector('edge')
+    .css({
+      'curve-style': 'bezier',
+      'target-arrow-shape': 'triangle',
+      width: 4,
+      'line-color': '#ddd',
+      'target-arrow-color': '#ddd',
+    })
+    .selector('.highlighted')
+    .css({
+      'background-color': '#61bffc',
+      'line-color': '#61bffc',
+      'target-arrow-color': '#61bffc',
+      'transition-property': 'background-color, line-color, target-arrow-color',
+      'transition-duration': '0.5s',
+    }),
 });
 
 
 /* get base network config */
 
-var query = {
-    "query": {
-        "filtered": {
-            "query": {
-                "match_all": {}
-            },
-            "filter": {
-                "range": {
-                    "timestamp": {
-                        "gt": "now-1h"
-                    }
-                }
-            }
-        }
-    }
-}
-
-
-axios.get('192.168.102.55:9200/network-1/log/_search',  { body: query })
-    .then(function (response) {
-        console.log(response);
-        /*
-        var eles = cy.add([
-            { group: "nodes", data: { id: "n0" }, position: { x: 100, y: 100 } },
-            { group: "nodes", data: { id: "n1" }, position: { x: 200, y: 200 } },
-            { group: "edges", data: { id: "e0", source: "n0", target: "n1" } }
-        ]);
-        */
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
-
-/*var bfs = cy.elements().bfs('#a', function () {}, true);
-
-var i = 0;
-var highlightNextEle = function () {
-    if (i < bfs.path.length) {
-        bfs.path[i].addClass('highlighted');
-
-        i++;
-        setTimeout(highlightNextEle, 1000);
-    }
+const query = {
+  size: 1,
+  sort: 'date:desc',
 };
 
-// kick off first highlight
-highlightNextEle();*/
+const lastNetworkTimestamp = new Date(1995, 11, 17);
+
+const randomSpawnPoint = () => ({
+  x: 500 + Math.floor((Math.random() * 300) + 150),
+  y: 200 + Math.floor((Math.random() * 300) + 150),
+});
+
+const getNetworkStatus = async () => axios.get('http://192.168.102.55:9200/network/log/_search', {
+  params: query,
+})
+  .then((response) => {
+    console.log(response);
+    if (lastNetworkTimestamp < new Date(response.data.hits.hits[0]._source.date)) {
+      cy.add([{
+        group: 'nodes',
+        data: {
+          id: 'ac',
+        },
+        position: randomSpawnPoint(),
+      },
+      {
+        group: 'edges',
+        data: {
+          id: 12,
+          source: 'ac',
+          target: 'k',
+        },
+      },
+      ]);
+    }
+    console.info('Network Config Request');
+    console.log(response);
+
+    setTimeout(() => {
+      getNetworkStatus();
+    }, 30000);
+  })
+  .catch((error) => {
+    console.log(error);
+    return error;
+  });
+
+const getMessages = () => axios.get('http://192.168.102.55:9200/messages-*/log/_search', {
+  params: query,
+})
+  .then((response) => {
+    console.info('Last Messages Request');
+    console.log(response);
+    /* var bfs = cy.elements().bfs('#a', function () {}, true);
+
+        var i = 0;
+        var highlightNextEle = function () {
+            if (i < bfs.path.length) {
+                bfs.path[i].addClass('highlighted');
+
+                i++;
+                setTimeout(highlightNextEle, 1000);
+            }
+        };
+
+        // kick off first highlight
+        highlightNextEle(); */
+    /*
+        var eles = cy.add([
+            
+        ]);
+        */
+
+    setTimeout(() => {
+      getMessages();
+    }, 1000);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+getNetworkStatus();
+// getMessages();
